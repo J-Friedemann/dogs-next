39d826069dfad0efa8225be4b2ea78303613fe4a
J-Friedemann <friedemann.jonatha@gmail.com>
2024-03-16 15:27:19 -0300

    add create account

--- src/app/login/perdeu/page.tsx
+++ src/app/login/perdeu/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Perdeu a senha | Dogs",
+  description: "Recupere a sua senha.",
+};
 export default async function PerdeuPage() {
   return (
     <main>
