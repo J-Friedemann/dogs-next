443be148f729fb7da2a5d16b1261eefe30752413
J-Friedemann <friedemann.jonatha@gmail.com>
2024-03-23 18:12:04 -0300

    feat: add photo comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import PhotosGet from "@/actions/photos-get";
+import photosGet from "@/actions/photos-get";
 import Feed from "@/components/feed/feed";
 
 export default async function Home() {
-  const { data } = await PhotosGet();
+  const { data } = await photosGet();
   return (
     <section className="container mainContainer">
       {data && <Feed photos={data} />}
