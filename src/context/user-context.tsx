420f09b80ee15e8bf0a6e12a4a8832dadca0456c
J-Friedemann <friedemann.jonatha@gmail.com>
2024-03-23 19:19:28 -0300

    feat: add validate token

--- src/context/user-context.tsx
+++ src/context/user-context.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import logout from "@/actions/logout";
+import validateToken from "@/actions/validate-token";
 import React from "react";
 
 type User = {
@@ -33,6 +35,13 @@ export function UserContextProvider({
 }) {
   const [userState, setUser] = React.useState<User | null>(user);
 
+  React.useEffect(() => {
+    async function validate() {
+      const { ok } = await validateToken();
+      if (!ok) await logout();
+    }
+    if (userState) validate();
+  }, [userState]);
   return (
     <UserContext.Provider value={{ user: userState, setUser }}>
       {children}
